94e2dd443c2b3954475fa685620eb3565b16887b
ZhaoYan <zhaoyan.hrb@gmail.com>
2010-10-11 13:56:15 +0800

    add two files

---
+++ FileNameTools.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include"FileNameTools.h"
+int GetDir(char *whole,char* dir)
+{
+    return 0;
+}
+
+
+
+int GetName(char *whole,char* Name)
+{
+    return 0;
+}
+
+
+int GetExt(char *whole,char* Ext)
+{
+    return 0;
+}
