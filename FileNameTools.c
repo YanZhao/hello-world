69f14eef7b6b6dbea443a3f62195ca4939e629ed
wangyilin <wangyi_lin2008@126.com>
2010-10-12 22:16:11 +0800

    wangyilin's work on getdir

--- FileNameTools.c
+++ FileNameTools.c
@@ -2,7 +2,13 @@
 #include<string.h>
 #include"FileNameTools.h"
 int GetDir(char *whole,char* dir)
-{
+{ 
+    int i,j;
+    memset(dir,0,sizeof(dir));
+    for(i=strlen(whole);1<=i;i--)
+      if(whole[i]=='\\')break;
+    for(j=0;j<i;j++)
+      dir[j]=whole[j];
     return 0;
 }
 
