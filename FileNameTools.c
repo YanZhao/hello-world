1b91edd878bfff68db3997e26f13b54e9a3a7c22
unknown <@.(none)>
2010-10-12 14:31:29 +0800

    wangzheng's work on Getdir

--- FileNameTools.c
+++ FileNameTools.c
@@ -3,18 +3,48 @@
 #include"FileNameTools.h"
 int GetDir(char *whole,char* dir)
 {
-    return 0;
+    int i,j;
+    char t;
+    for(i=0;i<=strlen(whole);i++)
+      if (whole[i]=='\\')
+      {
+          j=i;
+      }
+    t=whole[j+1];
+    whole[j+1]='\0';
+    strcpy(dir,whole);
+    whole[j+1]=t;
+    return j;
 }
 
 
 
 int GetName(char *whole,char* Name)
 {
-    return 0;
+    int i,j,k;
+    for(i=0;i<=strlen(whole);i++)
+    {
+        if (whole[i]=='\\')
+          j=i;
+        if (whole[i]=='.')
+          k=i;
+    }
+    for(i=j+1;i<k;i++)
+    {
+        strcat(Name,&whole[i]);
+    }
+    return k-j-1;
 }
 
 
 int GetExt(char *whole,char* Ext)
 {
-    return 0;
+    int i,j;
+    for(i=0;i<=strlen(whole);i++)
+     if (whole[i]=='.')
+     {
+         j=i;
+     }
+     strcpy(Ext,&whole[j+1]);
+    return strlen(whole)-j-1;
 }
